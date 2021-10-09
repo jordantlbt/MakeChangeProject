b2bdb2835f72d9e36e65f74e4fa7e77e0c707cca
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-10-08 18:08:00 -0600

    pennies, nickels

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -17,11 +17,6 @@ public class MakeChange {
 		
 		changeDue = cashGiven - amountDue;
 		
-		int pennies, nickles, dimes, quarters, dollars;
-		
-		
-		
-		
 		if(cashGiven < amountDue) {
 			System.out.println("The customer did not provide enough money. Please ask for ");
 			cash.nextLine();
@@ -31,14 +26,26 @@ public class MakeChange {
 			System.out.println("The customer gave exact amount due. No change needed.");
 		}
 		else{
-		
+			System.out.printf("The change due is $%.2f", changeDue );
 			
+			if (cashGiven < 5) {
+				
+				int pennies = (int)(changeDue / .01 + 1);
+				changeDue %= .01;
+				System.out.println("\nPlease give the customer " + pennies + " pennies");
+			}else if(cashGiven < 13) {
+				
+				int nickles = (int)(changeDue / .05 + 1);
+				changeDue %= .05;
+				System.out.println("\nPlease give the customer " + nickles + " nickles");
+			}
 			
-		System.out.printf("The change due is $%.2f", changeDue );
-		System.out.println();
 		}
 		
 		
+			
+			
+		}
 		
 	}
 
@@ -48,4 +55,3 @@ public class MakeChange {
 //	Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
 //	Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
 //	Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations for correct change.
-}
