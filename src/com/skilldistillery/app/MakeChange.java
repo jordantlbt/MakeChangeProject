a7a092044fdc32d857d4d2cc7a98b4c6d0541a3f
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-10-10 17:05:46 -0600

    might have figured out the correct change calculation

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -4,52 +4,66 @@ import java.util.Scanner;
 
 public class MakeChange {
 	public static void main(String[] args) {
-		
+
 		Scanner cash = new Scanner(System.in);
-		
-		
+
 		double amountDue, cashGiven, changeDue;
+
 		System.out.println("Please enter the total amount due: ");
 		amountDue = cash.nextDouble();
 		System.out.println("Please enter the amount of cash given: ");
 		cashGiven = cash.nextDouble();
 		cash.nextLine();
-		
+
 		changeDue = cashGiven - amountDue;
-		
-		if(cashGiven < amountDue) {
+
+		if (cashGiven < amountDue) {
 			System.out.println("The customer did not provide enough money. Please ask for ");
 			cash.nextLine();
-			
-		}
-		else if(cashGiven == amountDue) {
+
+		} else if (cashGiven == amountDue) {
 			System.out.println("The customer gave exact amount due. No change needed.");
 		}
-		else{
-			System.out.printf("The change due is $%.2f", changeDue );
-			
-			if (cashGiven < 5) {
-				
-				int pennies = (int)(changeDue / .01 + 1);
-				changeDue %= .01;
-				System.out.println("\nPlease give the customer " + pennies + " pennies");
-			}else if(cashGiven < 13) {
-				
-				int nickles = (int)(changeDue / .05 + 1);
-				changeDue %= .05;
-				System.out.println("\nPlease give the customer " + nickles + " nickles");
-			}
-			
-		}
-		
-		
-			
-			
-		}
-		
-	}
 
-	
+		// while (cashGiven > changeDue) {
+
+		System.out.printf("The change due is $%.2f", changeDue);
+
+		if (changeDue > 0) {
+			// if (changeDue % .25 > 0 || changeDue % .25 < 4) {
+
+			int quarters = (int) (changeDue / .25);
+
+			if (changeDue % .25 <= 1) {
+
+				System.out.println("\nPlease give the customer " + quarters + " quarter");
+			}
+			else if (changeDue % .25 <= 4 && changeDue > 1) {
+
+				System.out.println("\nPlease give the customer " + quarters + " quarters");
+			}
+			if (changeDue % .10 > 0 || changeDue % .10 < 10) {
+
+				int dimes = (int) (changeDue / .10);
+				System.out.println("\nPlease give the customer " + dimes + " dimes");
+			}
+
+			if (changeDue % .05 > 0 || changeDue % .05 <= 2) {
+
+				int nickles = (int) (changeDue / .05);
+				System.out.println("\nPlease give the customer " + nickles + " nickles");
+
+			}
+			if (changeDue % .01 > 0 || changeDue % .01 <= 4) {
+
+				int pennies = (int) (changeDue / .01 + 1);
+				System.out.println("\nPlease give the customer " + pennies + " pennies");
+				// break;
+			}
+		}
+	}
+}
+
 //	Amount: .67, Tendered: .50, Result: Error message
 //	Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
 //	Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
