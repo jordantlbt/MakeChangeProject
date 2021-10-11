ad1fa4c2d5b428af7de64e8acf727cfc2db869ce
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-10-10 23:18:49 -0600

    working project and read me

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Make Change Project
-<br>
-## Despription
-<br>
+
+## Description
+This project is my very first Java project.
+
+The user will enter the dollar and change amount in the correct USD format
+(example  00.00 ). The program should then be able to tell the user how much change is due and what dollar and coin types and quantities should be given back to the customer.
+
+User must re-run the program every time to enter new amounts currently.
+
 ### What I learned
-<br>
+I had a lot of difficulty getting the math correct to calculate change. I was able to search through multiple websites and try to calculate the math on my own, using trial and error, to find a combination that would get me the results I wanted.
+
+I learned a lot about if/else statements and how to make a variable continue through the statements even when it has fulfilled a statement.  
+
+
 ### Technologies used
-Java, Ecplise, Atom, GIT
+Java, Eclipse, Atom, GIT, Google
--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -8,6 +8,8 @@ public class MakeChange {
 		Scanner cash = new Scanner(System.in);
 
 		double amountDue, cashGiven, changeDue;
+		
+		//user enters correct amount including decimals
 
 		System.out.println("Please enter the total amount due: ");
 		amountDue = cash.nextDouble();
@@ -16,53 +18,107 @@ public class MakeChange {
 		cash.nextLine();
 
 		changeDue = cashGiven - amountDue;
-
+		
 		if (cashGiven < amountDue) {
-			System.out.println("The customer did not provide enough money. Please ask for ");
+			System.out.println("The customer did not provide enough money. Please ask for more");
 			cash.nextLine();
 
 		} else if (cashGiven == amountDue) {
 			System.out.println("The customer gave exact amount due. No change needed.");
 		}
-
-		// while (cashGiven > changeDue) {
-
+		else {
+		
 		System.out.printf("The change due is $%.2f", changeDue);
+		System.out.println("\n\nPlease give the customer: ");
+		
+		cash.close();
 
-		if (changeDue > 0) {
-			// if (changeDue % .25 > 0 || changeDue % .25 < 4) {
+			
+		//everything below is included in else statement
+		
+	
+		
+			int twentyDollar = (int) (changeDue / 20) ;
+			int tenDollar = (int) (changeDue % 20 / 10);
+			int fiveDollar = (int) (changeDue % 20 % 10 / 5);
+			int oneDollar = (int) (changeDue % 20 % 10 % 5);
+			
+				if (changeDue > 20 && changeDue <= 100) {
+	
+					System.out.println(twentyDollar + " twenty dollar bills");
+					changeDue = twentyDollar * 20;
+				}
+				if (changeDue >= 20 && changeDue < 40) {
+	
+					System.out.println(twentyDollar + " dollar bill");
+					changeDue -= twentyDollar * 20;
+				}
+				if (changeDue >= 10 && changeDue < 20) {
+				
+					System.out.println("1 ten dollar bill");
+					changeDue -= tenDollar * 10;
+				}
+				if (changeDue >= 5 && changeDue < 10) {
+					
+					System.out.println("1 five dollar bill");
+					changeDue -= fiveDollar * 5;	
+				}
+				if (changeDue < 2 && changeDue > .99) {
+					
+					System.out.println("1 one dollar bill");
+					changeDue -= 1;
+				}
+				if (changeDue >= 2 && changeDue < 5) {
+	
+					System.out.println(oneDollar + " one dollar bills");
+					changeDue = changeDue % oneDollar;
+				}
+	
+			
+			int quarters = (int) (changeDue / .25) ;
+			int dimes = (int) (changeDue % .25 / .10);
+			int nickels = (int) (changeDue % .25 % .10 / .05);
+			int pennies = (int) (changeDue % .25 % .10 % .05 / .01 + 1);
+			
+				if (changeDue > .50 && changeDue < 1) {
 
-			int quarters = (int) (changeDue / .25);
+					System.out.println(quarters + " quarters");
+					changeDue -= changeDue % quarters * 100;
+				}
+				if (changeDue >= .25 && changeDue < .50) {
 
-			if (changeDue % .25 <= 1) {
+					System.out.println(quarters + " quarter");
+					changeDue -= .25;
+				}
+				if (changeDue >= .20 && changeDue < .30) {
+				
+					System.out.println(dimes + " dimes");
+					changeDue -= changeDue % dimes * 100;
+				}
+				if	(changeDue >= .10 && changeDue < .20) {
+				
+					System.out.println(dimes + " dime");
+					changeDue -= .10;
+				}
+				if (changeDue > .04 && changeDue < .10) {
+					
+					System.out.println(nickels + " nickel");
+					changeDue -= .05;	
+				}
+				if (changeDue < .02) {
+					
+					System.out.println("1 penny");
+				}
+				if (changeDue >= .02 && changeDue < .05) {
 
-				System.out.println("\nPlease give the customer " + quarters + " quarter");
-			}
-			else if (changeDue % .25 <= 4 && changeDue > 1) {
-
-				System.out.println("\nPlease give the customer " + quarters + " quarters");
-			}
-			if (changeDue % .10 > 0 || changeDue % .10 < 10) {
-
-				int dimes = (int) (changeDue / .10);
-				System.out.println("\nPlease give the customer " + dimes + " dimes");
-			}
-
-			if (changeDue % .05 > 0 || changeDue % .05 <= 2) {
-
-				int nickles = (int) (changeDue / .05);
-				System.out.println("\nPlease give the customer " + nickles + " nickles");
-
-			}
-			if (changeDue % .01 > 0 || changeDue % .01 <= 4) {
-
-				int pennies = (int) (changeDue / .01 + 1);
-				System.out.println("\nPlease give the customer " + pennies + " pennies");
-				// break;
+					System.out.println(pennies + " pennies");
+			
+				}
+								
 			}
 		}
 	}
-}
+
 
 //	Amount: .67, Tendered: .50, Result: Error message
 //	Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
